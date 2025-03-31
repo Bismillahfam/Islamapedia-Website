f417f90571f1d52d6268596dc91b6a1859046117
Bismillahfam <ahsanmaleek1.27@gmail.com>
2025-03-31 12:52:30 +0100

    Dunnon

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
 }
 
 export default App;
