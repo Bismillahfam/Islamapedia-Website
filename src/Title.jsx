11c7a5dc161fadd70531e366506dfcc3f40e2853
Bismillahfam <ahsanmaleek1.27@gmail.com>
2025-03-31 14:12:33 +0100

    Added title card

--- src/Title.jsx
+++ src/Title.jsx
@@ -1,11 +1,11 @@
 import "./index.css";
-
-function Title() {
+function TitleCard() {
   return (
-    <>
-      <h1 className="bg-slate-600">Islamapedia</h1>
-    </>
+    <div className="w-96 bg-primary text-white dark:bg-primary dark:text-black p-6 rounded-b-2xl shadow-md text-center fixed top-0 left-1/2 -translate-x-1/2">
+      <h1 className="text-5xl font-bold">Islamapedia</h1>
+      <p className="text-md opacity-80">A Vault of Islamic Knowledge</p>
+    </div>
   );
 }
 
-export default Title;
+export default TitleCard;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,14 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}", // Ensures Tailwind scans all JSX files
   ],
+  darkMode: "media",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#978259",
+        primaryDark: "#756547",
+      },
+    },
   },
   plugins: [],
 };
