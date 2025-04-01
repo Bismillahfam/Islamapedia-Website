94669933885eb8a94efc6cb883e159c8ae478e21
Bismillahfam <ahsanmaleek1.27@gmail.com>
2025-04-01 19:44:27 +0100

    adjusted the rounded corners

--- src/MenuList.jsx
+++ src/MenuList.jsx
@@ -5,7 +5,7 @@ function MenuList(props) {
   const items = props.Items;
 
   return (
-    <div className="w-3/12 h-1/2 bg-[#8C7750] text-black rounded-tr-3xl shadow-xl p-8 fixed top-1/3 left-0 flex flex-col justify-center">
+    <div className="w-3/12 h-1/2 bg-[#8C7750] text-black rounded-br-3xl shadow-xl p-8 fixed top-1/3 left-0 flex flex-col justify-center">
       <ul className="text-3xl font-semibold space-y-6">
         <li className="p-4 rounded-lg hover:bg-[#6F6344] transition-all duration-300 transform hover:translate-x-5">
           {items[0]}
