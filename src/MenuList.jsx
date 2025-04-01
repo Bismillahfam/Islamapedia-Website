d348fad461fbca54223d2bf5a71c2a91c88a1f4a
Bismillahfam <ahsanmaleek1.27@gmail.com>
2025-04-01 19:42:57 +0100

    Adjusted the proportions of the title and menu

--- src/MenuList.jsx
+++ src/MenuList.jsx
@@ -5,15 +5,15 @@ function MenuList(props) {
   const items = props.Items;
 
   return (
-    <div className="bg-[#8C7750] text-white w-64 rounded-r-lg shadow-md p-4 fixed top-[144px] left-0">
-      <ul className="text-lg font-semibold space-y-2">
-        <li className="p-2 rounded-md hover:bg-[#6F6344] transition-all duration-300 transform hover:translate-x-3">
+    <div className="w-3/12 h-1/2 bg-[#8C7750] text-black rounded-tr-3xl shadow-xl p-8 fixed top-1/3 left-0 flex flex-col justify-center">
+      <ul className="text-3xl font-semibold space-y-6">
+        <li className="p-4 rounded-lg hover:bg-[#6F6344] transition-all duration-300 transform hover:translate-x-5">
           {items[0]}
         </li>
-        <li className="p-2 rounded-md hover:bg-[#6F6344] transition-all duration-300 transform hover:translate-x-3">
+        <li className="p-4 rounded-lg hover:bg-[#6F6344] transition-all duration-300 transform hover:translate-x-5">
           {items[1]}
         </li>
-        <li className="p-2 rounded-md hover:bg-[#6F6344] transition-all duration-300 transform hover:translate-x-3">
+        <li className="p-4 rounded-lg hover:bg-[#6F6344] transition-all duration-300 transform hover:translate-x-5">
           {items[2]}
         </li>
       </ul>
--- src/Title.jsx
+++ src/Title.jsx
@@ -2,9 +2,9 @@ import "./index.css";
 
 function Title() {
   return (
-    <div className="w-96 bg-primary text-white dark:bg-primary dark:text-black p-6 rounded-r-2xl shadow-lg text-center fixed top-0 left-0">
-      <h1 className="text-6xl font-bold">Islamapedia</h1>
-      <p className="text-lg opacity-80 mt-2">A Vault of Islamic Knowledge</p>
+    <div className="w-1/2 h-1/3 bg-primary text-white dark:bg-primary dark:text-black p-10 rounded-br-3xl shadow-xl text-center fixed top-0 left-0 flex flex-col justify-center">
+      <h1 className="text-7xl font-bold">Islamapedia</h1>
+      <p className="text-2xl opacity-80 mt-4">A Vault of Islamic Knowledge</p>
     </div>
   );
 }
