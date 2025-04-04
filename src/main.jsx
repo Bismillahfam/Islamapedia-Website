68ac67a136a1f413103703a675ba300f5279f9bf
Bismillahfam <ahsanmaleek1.27@gmail.com>
2025-04-04 17:02:56 +0100

    WIP Background video

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Title from "./Title.jsx";
 import MenuList from "./MenuList.jsx";
-import AuthenfticationBtn from "./AuthenticationBtn.jsx";
+import BackgroundVid from "./BackgroundVid.jsx";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
@@ -34,10 +34,6 @@ console.log(app);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthenfticationBtn />
-    <div className="min-h-screen bg-gradient-to-br from-[#EAD7A2] to-[#C2A878] text-[#4A3B27] flex justify-center items-center">
-      <Title />
-      <MenuList Items={["Add a Note", "Search Vault", "Buy me a Coffee"]} />
-    </div>
+    <BackgroundVid />
   </StrictMode>
 );
