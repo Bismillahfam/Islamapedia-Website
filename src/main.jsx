f515fd6fa82a6043ba075ab8f8023096a56a800f
Bismillahfam <ahsanmaleek1.27@gmail.com>
2025-04-04 10:33:42 +0100

    Hosting works

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,29 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Title from "./Title.jsx";
 import MenuList from "./MenuList.jsx";
-import GraphAnimation from "./GraphAnimation.jsx";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCgUpxkZ_soH8O2osTiN6qLZXLqvwVdO2E",
+  authDomain: "islamapedia-a43d7.firebaseapp.com",
+  projectId: "islamapedia-a43d7",
+  storageBucket: "islamapedia-a43d7.firebasestorage.app",
+  messagingSenderId: "590204114129",
+  appId: "1:590204114129:web:8b5faf113661c7bf4de1f0",
+  measurementId: "G-VGJTKTL5EM",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+console.log(app);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
