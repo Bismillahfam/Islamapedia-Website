356850908d3659dd54789d68d207e54440d73595
Bismillahfam <ahsanmaleek1.27@gmail.com>
2025-04-18 18:58:31 +0100

    added title, menu and auth button

--- src/Title.jsx
+++ src/Title.jsx
@@ -2,7 +2,7 @@ import "./index.css";
 
 function Title() {
   return (
-    <div className="w-1/2 h-1/3 bg-primary text-white dark:bg-primary dark:text-black p-10 rounded-br-3xl shadow-xl text-center fixed top-0 left-0 flex flex-col justify-center">
+    <div className="w-[52rem] h-1/3 bg-primary text-white dark:bg-primary dark:text-black p-10 rounded-br-3xl shadow-xl text-center flex flex-col justify-center">
       <h1 className="text-7xl font-bold">Islamapedia</h1>
       <p className="text-2xl opacity-80 mt-4">A Vault of Islamic Knowledge</p>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import Title from "./Title.jsx";
 import { GlobalProvider } from "./GlobalContext.jsx";
+import "./index.css";
+
+// Import components
+import Title from "./Title.jsx";
+import Menu from "./Menu.jsx";
+import AuthButton from "./AuthButton.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GlobalProvider>
       <Title />
+      <Menu items={["Add a Note", "Search Notes", "Donate"]} />
+      <AuthButton />
     </GlobalProvider>
   </StrictMode>
 );
